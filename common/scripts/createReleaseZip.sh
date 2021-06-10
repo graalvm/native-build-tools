5deb80f836c7a38c068e32dd64dad63cd4dc3c50
Lazar Mitrović <lazar.mitrovic@msn.com>
2021-06-10 03:05:05 +0200

    Add Github action to publish latest artifacts to releases

--- common/scripts/createReleaseZip.sh
+++ common/scripts/createReleaseZip.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/bash -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd $SCRIPT_DIR/../..
 
-rm -rd ~/.m2/repository/org/graalvm/buildtools
+rm -rdf ~/.m2/repository/org/graalvm/buildtools
 git commit -am "tmp"
 
 sed -i 's/-SNAPSHOT//' version.properties
 common/scripts/updateVersions.sh
 common/scripts/buildAll.sh
+common/scripts/testAll.sh
 
 git checkout --
 git reset --hard
