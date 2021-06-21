b4fc927f64ce878f8a50ce3db1c4f1427bdc4c86
Cédric Champeau <cedric.champeau@oracle.com>
2021-06-21 09:42:27 +0200

    Introduce test infra for the Gradle plugin
    This commit introduces functional tests for the Gradle plugin.
    For now, it's limited to a single test but it will be expanded for
    more use cases. Functional tests are using samples found in the
    `src/samples` directory.
    
    There are 2 test tasks which can be executed, depending on whether
    you want full test coverage or not:
    
    - `functionalTest` will run the test suite on the current version
    of Gradle only
    - `fullFunctionalTest` will do the same, but on all supported Gradle
    versions
    
    To make this possible, the build has been reorganized to leverage
    Gradle composite builds, instead of using the local Maven repository.
    Previously to this, it was necessary to cd into `junit-platform-native`,
    publish to maven Local, then go to the Gradle plugin, build it, and
    test with a local project.
    
    This is no longer required, as now the execution of functional tests
    will _automatically_ trigger the build of the native JUnit project,
    publish the library to a _test_ repository, which in turn is used by
    the functional tests (via repository injection in an init script).
    
    This makes it impossible to make mistakes, or have the build fragile
    if you don't build the `junit-platform-native` first: the dependency
    is now explicit.

---
+++ native-gradle-plugin/src/testFixtures/groovy/org/graalvm/buildtools/gradle/fixtures/TeeWriter.groovy
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2021, 2021 Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.graalvm.buildtools.gradle.fixtures
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class TeeWriter extends Writer {
+    private final Writer one
+    private final Writer two
+
+    static TeeWriter of(Writer one, Writer two) {
+        new TeeWriter(one, two)
+    }
+
+    private TeeWriter(Writer one, Writer two) {
+        this.one = one
+        this.two = two
+    }
+
+    @Override
+    void write(int c) throws IOException {
+        try {
+            one.write(c)
+        } finally {
+            two.write(c)
+        }
+    }
+
+    @Override
+    void write(char[] cbuf) throws IOException {
+        try {
+            one.write(cbuf)
+        } finally {
+            two.write(cbuf)
+        }
+    }
+
+    @Override
+    void write(char[] cbuf, int off, int len) throws IOException {
+        try {
+            one.write(cbuf, off, len)
+        } finally {
+            two.write(cbuf, off, len)
+        }
+    }
+
+    @Override
+    void write(String str) throws IOException {
+        try {
+            one.write(str)
+        } finally {
+            two.write(str)
+        }
+    }
+
+    @Override
+    void write(String str, int off, int len) throws IOException {
+        try {
+            one.write(str, off, len)
+        } finally {
+            two.write(str, off, len)
+        }
+    }
+
+    @Override
+    void flush() throws IOException {
+        try {
+            one.flush()
+        } finally {
+            two.flush()
+        }
+    }
+
+    @Override
+    void close() throws IOException {
+        try {
+            one.close()
+        } finally {
+            two.close()
+        }
+    }
+}
