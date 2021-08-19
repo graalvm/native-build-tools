2c286150aae177a08359a8d8f21a952609ba7d7f
Cedric Champeau <cedric.champeau@oracle.com>
2021-08-19 12:38:23 +0200

    Demonstrate how to run tests in shaded mode with Maven
    Unfortunately, due to different bugs(?) in Maven, we cannot use the
    shading plugin for tests, and we have to move the test execution to
    the integration test phase.
    
    See https://issues.apache.org/jira/browse/MSHADE-402

--- native-maven-plugin/src/functionalTest/groovy/org/graalvm/buildtools/maven/JavaApplicationWithTestsFunctionalTest.groovy
+++ native-maven-plugin/src/functionalTest/groovy/org/graalvm/buildtools/maven/JavaApplicationWithTestsFunctionalTest.groovy
@@ -66,4 +66,29 @@ class JavaApplicationWithTestsFunctionalTest extends AbstractGraalVMMavenFunctio
 [         0 tests failed          ]
 """.trim()
     }
+
+    def "can run tests in a native image with the Maven plugin using shading"() {
+        withSample("java-application-with-tests")
+
+        when:
+        mvn '-Pshaded', 'integration-test'
+
+        then:
+        buildSucceeded
+        outputContains "[junit-platform-native] Running in 'test listener' mode"
+        outputContains """
+[         3 containers found      ]
+[         0 containers skipped    ]
+[         3 containers started    ]
+[         0 containers aborted    ]
+[         3 containers successful ]
+[         0 containers failed     ]
+[         6 tests found           ]
+[         0 tests skipped         ]
+[         6 tests started         ]
+[         0 tests aborted         ]
+[         6 tests successful      ]
+[         0 tests failed          ]
+""".trim()
+    }
 }
