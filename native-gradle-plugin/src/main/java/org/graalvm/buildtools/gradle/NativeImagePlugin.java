417df253bddf16d403f6f01f3c346ff14756c313
Cedric Champeau <cedric.champeau@oracle.com>
2021-09-22 14:11:45 +0200

    Upgrade to JUnit Platform 5.8

--- native-gradle-plugin/src/main/java/org/graalvm/buildtools/gradle/NativeImagePlugin.java
+++ native-gradle-plugin/src/main/java/org/graalvm/buildtools/gradle/NativeImagePlugin.java
@@ -131,6 +131,7 @@ public class NativeImagePlugin implements Plugin<Project> {
      */
     private static final Consumer<Object> FORCE_CONFIG = t -> {
     };
+    private static final String JUNIT_PLATFORM_LISTENER_ENABLE = "junit.platform.listeners.uid.tracking.enabled";
     private static final String JUNIT_PLATFORM_LISTENERS_UID_TRACKING_OUTPUT_DIR = "junit.platform.listeners.uid.tracking.output.dir";
 
     private GraalVMLogger logger;
@@ -239,6 +240,7 @@ public class NativeImagePlugin implements Plugin<Project> {
             testListDirectory.set(new File(testResultsDir, test.getName() + "/testlist"));
             test.getOutputs().dir(testResultsDir);
             // Set system property read by the UniqueIdTrackingListener.
+            test.systemProperty(JUNIT_PLATFORM_LISTENER_ENABLE, true);
             test.systemProperty(JUNIT_PLATFORM_LISTENERS_UID_TRACKING_OUTPUT_DIR, testListDirectory.getAsFile().get());
             configureAgent(project, testAgent, testOptions, testTask.named(test.getName()), processAgentTestFiles);
             test.doFirst("cleanup test ids", new CleanupTestIdsDirectory(testListDirectory));
@@ -469,7 +471,7 @@ public class NativeImagePlugin implements Plugin<Project> {
     private static void injectTestPluginDependencies(Project project, Property<Boolean> testSupportEnabled) {
         project.afterEvaluate(p -> {
             if (testSupportEnabled.get()) {
-                project.getDependencies().add("implementation", "org.graalvm.buildtools:junit-platform-native:"
+                project.getDependencies().add("testImplementation", "org.graalvm.buildtools:junit-platform-native:"
                         + VersionInfo.JUNIT_PLATFORM_NATIVE_VERSION);
             }
         });
