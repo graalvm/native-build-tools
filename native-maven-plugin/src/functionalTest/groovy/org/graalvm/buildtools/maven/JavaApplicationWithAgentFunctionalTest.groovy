d390e26735b4848b70eabfc069c80b6ccf35256e
Sam Brannen <sbrannen@vmware.com>
2021-11-22 12:23:09 +0100

    Support custom agent options in Maven plugin
    Prior to this commit, users could not supply custom options to the JVM
    agent. In addition, the `experimental-class-loader-support` option was
    always supplied, which may not be desired by all users.
    
    This commit introduces support for configuring the agent by allowing
    users to specify multiple sets of named `agentOptions` in the
    `configuration` block for the native-maven-plugin.
    
    The following example configures two sets of agent options, one for
    execution of the application (exec) and one for tests (test).
    
    <configuration>
      <agentOptions name="exec">
        <agentOption>experimental-class-loader-support</agentOption>
      </agentOptions>
      <agentOptions name="test">
        <agentOption>experimental-class-loader-support</agentOption>
        <agentOption>access-filter-file=${basedir}/src/test/resources/access-filter.json</agentOption>
      </agentOptions>
    </configuration>
    
    In addition, the `experimental-class-loader-support` option is no
    longer configured automatically by the native-maven-plugin.
    
    See #162

--- native-maven-plugin/src/functionalTest/groovy/org/graalvm/buildtools/maven/JavaApplicationWithAgentFunctionalTest.groovy
+++ native-maven-plugin/src/functionalTest/groovy/org/graalvm/buildtools/maven/JavaApplicationWithAgentFunctionalTest.groovy
@@ -46,7 +46,7 @@ import spock.lang.Unroll
 
 class JavaApplicationWithAgentFunctionalTest extends AbstractGraalVMMavenFunctionalTest {
 
-    def "agent is passed"() {
+    def "agent is used without custom options"() {
         given:
         withSample("java-application-with-reflection")
 
@@ -73,21 +73,107 @@ class JavaApplicationWithAgentFunctionalTest extends AbstractGraalVMMavenFunctio
         ['jni', 'proxy', 'reflect', 'resource', 'serialization'].each { name ->
             assert file("target/native/agent-output/test/${name}-config.json").exists()
         }
+
+        // If the custom access-filter.json is NOT applied, we should see a warning similar to the following.
+        // Warning: Could not resolve org.apache.maven.surefire.junitplatform.JUnitPlatformProvider for reflection configuration. Reason: java.lang.ClassNotFoundException: org.apache.maven.surefire.junitplatform.JUnitPlatformProvider.
+        and:
+        outputContains 'Warning: Could not resolve org.apache.maven.surefire'
+    }
+
+    def "agent is used with custom options"() {
+        given:
+        withSample("java-application-with-reflection")
+
+        when:
+        // Run Maven in debug mode (-X) in order to capture the command line arguments
+        // used to launch Surefire with the agent.
+        mvn '-X', '-Pnative', '-Dagent=true', '-DagentOptions=test', 'test'
+
+        then:
+        outputContains """
+[         4 containers found      ]
+[         0 containers skipped    ]
+[         4 containers started    ]
+[         0 containers aborted    ]
+[         4 containers successful ]
+[         0 containers failed     ]
+[         7 tests found           ]
+[         0 tests skipped         ]
+[         7 tests started         ]
+[         0 tests aborted         ]
+[         7 tests successful      ]
+[         0 tests failed          ]
+""".trim()
+
+        and:
+        ['jni', 'proxy', 'reflect', 'resource', 'serialization'].each { name ->
+            assert file("target/native/agent-output/test/${name}-config.json").exists()
+        }
+
+        and:
+        // If custom agent options are processed, the debug output for Surefire
+        // should include the following segments of the agent command line argument.
+        // -agentlib:native-image-agent=experimental-class-loader-support,access-filter-file=<BUILD_DIR>/src/test/resources/access-filter.json,config-output-dir=<BUILD_DIR>/target/native/agent-output/test
+        outputContains '-agentlib:native-image-agent=experimental-class-loader-support,access-filter-file='
+        outputContains '/src/test/resources/access-filter.json'.replace('/', java.io.File.separator)
+        outputContains ',config-output-dir='
+        outputContains '/target/native/agent-output/test'.replace("/", java.io.File.separator)
+
+        and:
+        // If the custom access-filter.json is applied, we should not see any warnings about Surefire types.
+        // The actual warning would be something like:
+        // Warning: Could not resolve org.apache.maven.surefire.junitplatform.JUnitPlatformProvider for reflection configuration. Reason: java.lang.ClassNotFoundException: org.apache.maven.surefire.junitplatform.JUnitPlatformProvider.
+        outputDoesNotContain 'Warning: Could not resolve org.apache.maven.surefire'
     }
 
     @Issue("https://github.com/graalvm/native-build-tools/issues/134")
-    @Unroll("generated agent files are added when building native image on Gradle #version with JUnit Platform #junitVersion")
+    @Unroll("generated agent files are added when building native image on Maven #version with JUnit Platform #junitVersion")
     def "generated agent files are used when building native image"() {
         given:
         withSample("java-application-with-reflection")
 
         when:
-        mvn '-Pnative', '-Dagent=true', '-DskipTests=true', '-DskipNativeBuild=true', 'package', 'exec:exec@java-agent'
+        mvn '-X', '-Pnative', '-Dagent=true', '-DskipTests=true', '-DskipNativeBuild=true', 'package', 'exec:exec@java-agent'
 
         then:
         ['jni', 'proxy', 'reflect', 'resource', 'serialization'].each { name ->
             assert file("target/native/agent-output/exec/${name}-config.json").exists()
         }
+
+        and:
+        // If custom agent options are not used, the Maven debug output should include
+        // the following segments of the agent command line argument.
+        // -agentlib:native-image-agent=config-output-dir=<BUILD_DIR>/target/native/agent-output/exec
+        outputContains '-agentlib:native-image-agent=config-output-dir='
+        outputContains '/target/native/agent-output/exec'.replace("/", java.io.File.separator)
+        outputDoesNotContain 'experimental-class-loader-support'
+
+        when:
+        mvn '-Pnative', '-Dagent=true', '-DskipTests=true', 'package', 'exec:exec@native'
+
+        then:
+        outputContains "Hello, native!"
+    }
+
+    def "custom options and generated agent files are used when building native image"() {
+        given:
+        withSample("java-application-with-reflection")
+
+        when:
+        mvn '-X', '-Pnative', '-Dagent=true', '-DagentOptions=exec', '-DskipTests=true', '-DskipNativeBuild=true', 'package', 'exec:exec@java-agent'
+
+        then:
+        ['jni', 'proxy', 'reflect', 'resource', 'serialization'].each { name ->
+            assert file("target/native/agent-output/exec/${name}-config.json").exists()
+        }
+
+        and:
+        // If custom agent options are used, the Maven debug output should include
+        // the following segments of the agent command line argument.
+        // -agentlib:native-image-agent=experimental-class-loader-support,config-output-dir=<BUILD_DIR>/target/native/agent-output/exec
+        outputContains '-agentlib:native-image-agent=experimental-class-loader-support,config-output-dir='
+        outputContains '/target/native/agent-output/exec'.replace("/", java.io.File.separator)
+        outputDoesNotContain 'access-filter-file='
 
         when:
         mvn '-Pnative', '-Dagent=true', '-DskipTests=true', 'package', 'exec:exec@native'
