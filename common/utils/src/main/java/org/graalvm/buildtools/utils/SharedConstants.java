63b8db1d655fd0866d7aa0aa84ee502c114fbbb7
David Nestorovic <david.nestorovic@oracle.com>
2022-10-10 16:07:35 +0200

    Bump the default metadata repository version to 0.2.2

--- common/utils/src/main/java/org/graalvm/buildtools/utils/SharedConstants.java
+++ common/utils/src/main/java/org/graalvm/buildtools/utils/SharedConstants.java
@@ -76,5 +76,5 @@ public interface SharedConstants {
     String AGENT_OUTPUT_DIRECTORY_MARKER = "{output_dir}";
     String AGENT_OUTPUT_DIRECTORY_OPTION = "config-output-dir=";
     String METADATA_REPO_URL_TEMPLATE = "https://github.com/oracle/graalvm-reachability-metadata/releases/download/%1$s/graalvm-reachability-metadata-%1$s.zip";
-    String METADATA_REPO_DEFAULT_VERSION = "0.2.1";
+    String METADATA_REPO_DEFAULT_VERSION = "0.2.2";
 }
