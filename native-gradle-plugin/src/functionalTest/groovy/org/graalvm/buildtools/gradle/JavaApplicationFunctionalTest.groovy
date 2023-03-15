908113e23672a8195fe7315e9293f3d0b16ef3dc
Cedric Champeau <cedric.champeau@oracle.com>
2023-03-15 08:37:25 +0100

    Disable test

--- native-gradle-plugin/src/functionalTest/groovy/org/graalvm/buildtools/gradle/JavaApplicationFunctionalTest.groovy
+++ native-gradle-plugin/src/functionalTest/groovy/org/graalvm/buildtools/gradle/JavaApplicationFunctionalTest.groovy
@@ -42,8 +42,7 @@
 package org.graalvm.buildtools.gradle
 
 import org.graalvm.buildtools.gradle.fixtures.AbstractFunctionalTest
-import org.gradle.util.GradleVersion
-import spock.lang.IgnoreIf
+import spock.lang.Ignore
 import spock.lang.Issue
 import spock.lang.Requires
 
@@ -115,28 +114,20 @@ class JavaApplicationFunctionalTest extends AbstractFunctionalTest {
         def graalvmHome = System.getenv("GRAALVM_HOME")
         graalvmHome != null
     })
-    @IgnoreIf({
-        // Incompatible with configuration cache because the toolchain error causes serialization
-        // problems
-        Boolean.getBoolean("config.cache")
-    })
+    @Ignore("Need to find another way to test this since toolchains will now always be evaluated early")
     def "can override toolchain selection"() {
         def nativeApp = file("build/native/nativeCompile/java-application")
-        boolean dummyToolchain = GradleVersion.version(gradleVersion).compareTo(GradleVersion.version("7.0")) >= 0
 
         given:
         withSample("java-application")
 
-        if (dummyToolchain) {
             buildFile << """graalvmNative.binaries.configureEach {
                 javaLauncher.set(javaToolchains.launcherFor {
                     languageVersion.set(JavaLanguageVersion.of(JavaVersion.current().getMajorVersion()))
                     vendor.set(JvmVendorSpec.matching("non existing vendor"))
                 })
-            }"""
-        }
-
-        buildFile << """
+            }
+            
             tasks.withType(org.graalvm.buildtools.gradle.tasks.BuildNativeImageTask).configureEach {
                 disableToolchainDetection = true
             }
