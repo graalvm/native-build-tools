bbb9daca28c5b9f08a153b792829e37519fa3a56
Fabio Niephaus <fabio.niephaus@oracle.com>
2023-09-11 16:40:29 +0200

    Update JUnit config for GraalVM for JDK 21.

--- common/junit-platform-native/src/main/java/org/graalvm/junit/platform/config/vintage/VintageConfigProvider.java
+++ common/junit-platform-native/src/main/java/org/graalvm/junit/platform/config/vintage/VintageConfigProvider.java
@@ -58,6 +58,19 @@ public class VintageConfigProvider implements PluginConfigProvider {
                 /* Workaround until we can register serializable classes from a native-image feature */
                 "org.junit.runner.Result"
         );
+
+        if (getMajorJDKVersion() >= 21) {
+            /* new with simulated class initialization */
+            config.initializeAtBuildTime(
+                    "java.lang.annotation.Annotation",
+                    "org.junit.runners.model.FrameworkMethod",
+                    "org.junit.runners.model.TestClass",
+                    "org.junit.runners.ParentRunner$1",
+                    "org.junit.Test",
+                    "org.junit.vintage.engine.descriptor.VintageEngineDescriptor",
+                    "org.junit.vintage.engine.VintageTestEngine"
+            );
+        }
     }
 
     @Override
