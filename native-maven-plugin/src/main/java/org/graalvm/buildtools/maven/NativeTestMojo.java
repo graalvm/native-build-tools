095a84d1a885aca3817a10454ffd6389fed11264
David Nestorovic <david.nestorovic@oracle.com>
2025-04-09 18:03:51 +0200

    Use dependencies pom files to get gav coordinates required for filtering

--- native-maven-plugin/src/main/java/org/graalvm/buildtools/maven/NativeTestMojo.java
+++ native-maven-plugin/src/main/java/org/graalvm/buildtools/maven/NativeTestMojo.java
@@ -61,7 +61,12 @@ import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 import org.eclipse.aether.resolution.DependencyResult;
 import org.graalvm.buildtools.utils.NativeImageConfigurationUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -132,6 +137,8 @@ public class NativeTestMojo extends AbstractNativeImageMojo {
             .filter(it -> it.getGroupId().startsWith(NativeImageConfigurationUtils.MAVEN_GROUP_ID) || it.getGroupId().startsWith("org.junit"))
             .map(it -> it.getFile().toPath())
             .forEach(imageClasspath::add);
+
+        modules.addAll(collectJUnitModulesAlreadyOnClasspath());
         var jars = findJunitPlatformNativeJars(modules);
         imageClasspath.addAll(jars);
     }
@@ -298,17 +305,53 @@ public class NativeTestMojo extends AbstractNativeImageMojo {
             .stream()
             .map(ArtifactResult::getArtifact)
             .filter(a -> !modulesAlreadyOnClasspath.contains(new Module(a.getGroupId(), a.getArtifactId())))
-            .filter(a -> imageClasspath.stream().noneMatch(entry -> matchGroup(entry, a.getGroupId()) && matchArtifact(entry, a.getArtifactId())))
             .map(a -> a.getFile().toPath())
             .collect(Collectors.toList());
     }
 
-    private boolean matchGroup(Path entry, String groupId) {
-        return entry.toString().contains(groupId.replace(".", File.separator));
+    private Set<Module> collectJUnitModulesAlreadyOnClasspath() {
+        Set<Module> artifacts = new HashSet<>();
+        for (Path entry : imageClasspath) {
+            if (isJUnitArtifact(entry)) {
+                File pom = getArtifactPOM(entry);
+                if (pom != null) {
+                    artifacts.add(getModuleFromPOM(pom));
+                }
+            }
+        }
+
+        return artifacts;
     }
 
-    private boolean matchArtifact(Path entry, String artifactId) {
-        return entry.toString().contains(artifactId);
+    private boolean isJUnitArtifact(Path entry) {
+        return entry.toString().contains("junit");
+    }
+
+    private File getArtifactPOM(Path classpathEntry) {
+        List<File> artifactContent = getArtifactContent(classpathEntry.getParent());
+        List<File> candidates = artifactContent.stream().filter(f -> f.getName().endsWith(".pom")).collect(Collectors.toList());
+        return candidates.size() != 1 ? null : candidates.get(0);
+    }
+
+    private List<File> getArtifactContent(Path path) {
+        File[] content = path.toFile().listFiles();
+        return content == null ? List.of() : List.of(content);
+    }
+
+    private Module getModuleFromPOM(File pom) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringElementContentWhitespace(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(pom);
+
+            String groupId = doc.getElementsByTagName("groupId").item(0).getFirstChild().getTextContent();
+            String artifactId = doc.getElementsByTagName("artifactId").item(0).getFirstChild().getTextContent();
+
+            return new Module(groupId, artifactId);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            throw new RuntimeException("Cannot get maven coordinates from " + pom.getPath() + ". Reason: " + e.getMessage());
+        }
     }
 
     private static final class Module {
