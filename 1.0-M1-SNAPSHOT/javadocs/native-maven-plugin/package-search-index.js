98f621463bc41723fcb05848f938e13d33ce6e3d
jvukicev <jovan.vukicevic@oracle.com>
2026-04-09 10:32:18 +0200

    Update gh-pages latest link and remove invalid releases

--- 1.0-M1-SNAPSHOT/javadocs/native-maven-plugin/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.graalvm.buildtools.maven"},{"l":"org.graalvm.buildtools.maven.config"},{"l":"org.graalvm.buildtools.maven.config.agent"},{"l":"org.graalvm.buildtools.maven.sbom"},{"l":"org.graalvm.buildtools.utils"}];updateSearchResults();
