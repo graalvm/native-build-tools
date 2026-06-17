a49c969ad5998a1944ef3c8e3e38854832d1530b
jvukicev <jovan.vukicevic@oracle.com>
2026-06-17 11:18:27 +0200

    Fix optional JUnit dependency resolution in Maven native tests

---
+++ native-maven-plugin/reproducers/issue-756/src/test/java/org/graalvm/buildtools/examples/SimpleTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2026, 2026, Oracle and/or its affiliates.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ */
+package org.graalvm.buildtools.examples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleTest {
+    @Test
+    void succeeds() {
+        assertTrue(true);
+    }
+}
