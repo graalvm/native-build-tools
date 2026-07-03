891c8b177f769f131282aabcad6649d3bfbefe84
jvukicev <jovan.vukicevic@oracle.com>
2026-07-03 16:15:12 +0200

    Revert layered classpath input change

--- native-gradle-plugin/src/main/java/org/graalvm/buildtools/gradle/internal/BaseNativeImageOptions.java
+++ native-gradle-plugin/src/main/java/org/graalvm/buildtools/gradle/internal/BaseNativeImageOptions.java
@@ -446,8 +446,6 @@ public abstract class BaseNativeImageOptions implements NativeImageOptions {
         }
         layer.getLayerName().convention(binaryName);
         spec.execute(layer);
-        // The dependency layer is defined exclusively by its configured jars. §E2E-functional-tests.
-        getClasspath().setFrom(layer.getJars());
         layers(options -> options.add(layer));
     }
 }
