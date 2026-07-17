547a5210a4f73b846de19159e1f5a1cce9751cd6
jvukicev <jovan.vukicevic@oracle.com>
2026-07-17 15:33:25 +0200

    Use portable paths for Gradle config exclusions

--- native-gradle-plugin/src/main/java/org/graalvm/buildtools/gradle/NativeImagePlugin.java
+++ native-gradle-plugin/src/main/java/org/graalvm/buildtools/gradle/NativeImagePlugin.java
@@ -1070,7 +1070,8 @@ public class NativeImagePlugin implements Plugin<Project> {
     private static void addExcludeConfigArg(List<String> args, Path jarPath, List<String> listOfResourcePatterns) {
         listOfResourcePatterns.forEach(resourcePattern -> {
             args.add("--exclude-config");
-            args.add(Pattern.quote(jarPath.toAbsolutePath().toString()));
+            // Native Image receives portable path separators in exclude-config regular expressions. §FS-resources-and-metadata.3.
+            args.add(Pattern.quote(jarPath.toAbsolutePath().toString().replace(File.separatorChar, '/')));
             args.add(String.format("%s", resourcePattern));
         });
     }
--- native-gradle-plugin/src/test/groovy/org/graalvm/buildtools/gradle/NativeImagePluginTest.groovy
+++ native-gradle-plugin/src/test/groovy/org/graalvm/buildtools/gradle/NativeImagePluginTest.groovy
@@ -105,7 +105,7 @@ class NativeImagePluginTest extends Specification {
         qa.classpath.files.containsAll(classpathConfiguration.files)
         qa.excludeConfigArgs.get() == [
                 "--exclude-config",
-                Pattern.quote(testJar.toPath().toAbsolutePath().toString()),
+                Pattern.quote(testJar.toPath().toAbsolutePath().toString().replace(File.separator, '/')),
                 "META-INF/*"
         ]
     }
